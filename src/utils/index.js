4d55a9c36a59f9bb7be479d08d7e701c85d15182
Radek Titěra <rodak.titera@gmail.com>
2024-12-29 00:03:08 +0100

    Fix: Not exporting Collision class from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { AreaImage } from "./areaImage.js";
 export { Grid2D } from "./grid2D.js";
 export { JsonAsset } from "./jsonAsset.js";
 export { Sprite } from "./sprite.js";
+export { Collision } from './collision.js'
