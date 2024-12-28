11bf35ad5a7466067616b041d6429b4e6523c3af
Radek Titěra <rodak.titera@gmail.com>
2024-12-28 13:19:27 +0100

    Removed TSC

--- dist/core/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Font } from "./font.js";
-export { Image } from "./image.js";
-export { Input } from "./input.js";
-export { Shader } from "./shader/shader.js";
-export { ShaderLayer } from "./shader/shaderLayer.js";
-export { Time } from "./time.js";
-export { Sketch } from "./sketch.js";
-//# sourceMappingURL=index.d.ts.map
--- dist/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { AreaImage } from "./areaImage.js";
-export { Grid2D } from "./grid2D.js";
-export { JsonAsset } from "./jsonAsset.js";
-export { Sprite } from "./sprite.js";
-//# sourceMappingURL=index.d.ts.map
